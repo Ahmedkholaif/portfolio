743590774596048cce7472d30cb2c129d6531cab
ahmedkholaif <ahmed_kholaif@yahoo.com>
2020-01-18 15:15:04 +0200

    Updates

---
+++ precache-manifest.fdd4e245003e07a81f58280dbab55e2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d89701c019290cdf1ed086a7cdc71837",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a3e564a02400aa368174",
+    "url": "/portfolio/static/css/main.e2473e87.chunk.css"
+  },
+  {
+    "revision": "df24907c8f90f93452c4",
+    "url": "/portfolio/static/js/2.91caffe4.chunk.js"
+  },
+  {
+    "revision": "a3e564a02400aa368174",
+    "url": "/portfolio/static/js/main.8e133c89.chunk.js"
+  },
+  {
+    "revision": "c5306228cc83f6f6271c",
+    "url": "/portfolio/static/js/runtime-main.7676bd13.js"
+  }
+]);
